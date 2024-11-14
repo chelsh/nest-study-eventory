33440f5d3f9ad131c2223332c84e329f329659f3
chelsh <sinye2002@korea.ac.kr>
2024-11-15 00:02:02 +0900

    ✨ Week5/signUp API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { TokenDto } from './dto/token.dto';
+import { SignUpPayload } from './payload/sign-up.payload';
 
 @Controller('auth')
 @ApiTags('Auth API')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-up')
+  @ApiOperation({ summary: '회원가입' })
+  @ApiCreatedResponse({ type: TokenDto })
+  async signUp(
+    @Body() payload: SignUpPayload,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<TokenDto> {
+    const tokens = await this.authService.signUp(payload);
+
+    // refresh Token은 쿠키로
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      // 이후 실제 도메인으로 변경
+      domain: 'localhost',
+    });
+
+    return TokenDto.from(tokens.accessToken);
+  }
 }
