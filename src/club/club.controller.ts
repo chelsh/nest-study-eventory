7b3901cbc1f05e989e7a09990109138cf41be084
chelsh <sinye2002@korea.ac.kr>
2024-12-02 18:23:10 +0900

    ✨ Create club

---
+++ src/club/club.controller.ts
@@ -0,0 +1,31 @@
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ClubService } from './club.service';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { ClubDto } from './dto/club.dto';
+import { UserBaseInfo } from 'src/auth/type/user-base-info.type';
+import { CurrentUser } from 'src/auth/decorator/user.decorator';
+import { CreateClubPayload } from './payload/create-club.payload';
+
+@Controller('clubs')
+@ApiTags('Club API')
+export class ClubController {
+  constructor(private readonly clubService: ClubService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '이벤트를 생성합니다' })
+  @ApiCreatedResponse({ type: ClubDto })
+  async createEvent(
+    @CurrentUser() user: UserBaseInfo,
+    @Body() payload: CreateClubPayload,
+  ): Promise<ClubDto> {
+    return this.clubService.createClub(user.id, payload);
+  }
+}
---
+++ src/club/dto/club.dto.ts
@@ -0,0 +1,62 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ClubData } from '../type/club-data.type';
+
+export class ClubDto {
+  @ApiProperty({
+    description: '클럽 ID',
+    type: Number,
+  })
+  id!: number;
+
+  @ApiProperty({
+    description: '클럽장 ID',
+    type: Number,
+  })
+  hostId!: number;
+
+  @ApiProperty({
+    description: '클럽 이름',
+    type: String,
+  })
+  name!: string;
+
+  @ApiProperty({
+    description: '클럽 소개',
+    type: String,
+  })
+  description!: string;
+
+  @ApiProperty({
+    description: '클럽 최대 인원',
+    type: Number,
+  })
+  maxPeople!: number;
+
+  static from(club: ClubData): ClubDto {
+    return {
+      id: club.id,
+      hostId: club.hostId,
+      name: club.name,
+      description: club.description,
+      maxPeople: club.maxPeople,
+    };
+  }
+
+  static fromArray(clubs: ClubData[]): ClubDto[] {
+    return clubs.map((event) => this.from(event));
+  }
+}
+
+export class ClubListDto {
+  @ApiProperty({
+    description: '이벤트 목록',
+    type: [ClubDto],
+  })
+  clubs!: ClubDto[];
+
+  static from(clubs: ClubData[]): ClubListDto {
+    return {
+      clubs: ClubDto.fromArray(clubs),
+    };
+  }
+}
