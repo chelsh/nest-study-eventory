599d4c7cbdb620cb8242ed9bd88dd6ddc40d7334
chelsh <sinye2002@korea.ac.kr>
2024-12-03 21:55:44 +0900

    ✨ Join/Out Club

--- src/club/club.controller.ts
+++ src/club/club.controller.ts
@@ -1,11 +1,20 @@
 import {
   ApiBearerAuth,
   ApiCreatedResponse,
+  ApiNoContentResponse,
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
 import { ClubService } from './club.service';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 import { ClubDto } from './dto/club.dto';
 import { UserBaseInfo } from 'src/auth/type/user-base-info.type';
@@ -27,5 +36,33 @@ export class ClubController {
     @Body() payload: CreateClubPayload,
   ): Promise<ClubDto> {
     return this.clubService.createClub(user.id, payload);
+  }
+
+  @Post(':clubId/join')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @HttpCode(204)
+  @ApiOperation({ summary: '클럽에 가입 신청합니다.' })
+  @ApiNoContentResponse()
+  async joinEvent(
+    @CurrentUser() user: UserBaseInfo,
+    @Param('clubId', ParseIntPipe) clubId: number,
+  ): Promise<void> {
+    return this.clubService.joinClub(clubId, user.id);
+  }
+
+  @Post(':clubId/out')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @HttpCode(204)
+  @ApiOperation({
+    summary: '클럽 가입 요청을 철회하거나, 클럽에서 탈퇴합니다.',
+  })
+  @ApiNoContentResponse()
+  async outEvent(
+    @CurrentUser() user: UserBaseInfo,
+    @Param('clubId', ParseIntPipe) clubId: number,
+  ): Promise<void> {
+    return this.clubService.outClub(clubId, user.id);
   }
 }
